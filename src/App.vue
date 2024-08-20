4c73ecc54c903fffb828dff044a68a4754557998
Billy Charlton <mail@billyc.cc>
2024-08-20 11:00:13 +0200

    home locs and hexagon aggregation both working

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template lang="pug">
 .app
-    p {{ statusText}}
-    .mymap(id="mymap")
+    .flex-row
+      p.flex1(style="margin: auto 0"): b {{ statusText }}
+      button(@click="view=1" :class="{isActive: view==1}") Aggregate
+      button(@click="view=0" :class="{isActive: view==0}") Home locations
+    .mymap.flex1(id="mymap")
 
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Papa from '@simwrapper/papaparse'
-// import { Deck } from '@deck.gl/core'
 import { MapboxOverlay as DeckOverlay } from '@deck.gl/mapbox'
 import { ScatterplotLayer } from '@deck.gl/layers'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
+
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
-import Coords from './Coords'
-
 const DATA_URL = 'http://localhost:8000/infections-map'
-const INFECTIONS_URL = `${DATA_URL}/calibration481.infectionEvents.txt`
-const POPULATION_URL = `${DATA_URL}/population.csv`
-const PROJECTION = 'EPSG:25832'
+const INFECTIONS_URL = `${DATA_URL}/infections.csv` // 'calibration481.infectionEvents.txt`
 
 interface InfectionRecord {
   date: string
@@ -31,7 +31,13 @@ interface InfectionRecord {
   facility?: string
   virusStrain?: string
   probability?: number
-  coords: number[]
+  home_lon: number
+  home_lat: number
+}
+
+enum MapView {
+  points,
+  hexagons,
 }
 
 export default defineComponent({
@@ -45,129 +51,143 @@ export default defineComponent({
       population: [] as any[],
       numInfections: 0,
       statusText: 'Loading...',
+      coordinates: new Float64Array(1),
+      view: MapView.hexagons,
+      deckOverlay: {} as any,
     }
   },
   computed: {},
-  watch: {},
+  watch: {
+    view() {
+      this.updateLayers()
+    },
+  },
+
+  beforeDestroy() {
+    this.allInfections = []
+    this.coordinates = new Float64Array(1)
+    this.population = []
+  },
 
   mounted() {
     this.map = new maplibregl.Map({
       container: 'mymap',
       style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
-      center: [5, 50],
-      zoom: 5,
+      // style: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
+      center: [6.95, 50.9],
+      zoom: 10,
       bearing: 0,
-      pitch: 0,
+      pitch: 50,
     })
 
-    this.loadPopulation()
+    this.loadInfections()
   },
 
   methods: {
-    loadPopulation() {
-      Papa.parse(POPULATION_URL, {
-        // preview: 10000,
-        download: true,
-        header: true,
-        dynamicTyping: false,
-        delimiter: ',',
-        skipEmptyLines: true,
-        complete: (results: any, _: any) => {
-          this.population = results.data
-          this.loadInfections()
-        },
-      })
-    },
-
     loadInfections() {
       Papa.parse(INFECTIONS_URL, {
-        preview: 200000,
+        // preview: 250000,
         download: true,
         header: true,
-        dynamicTyping: false,
+        dynamicTyping: true,
         worker: true,
-        delimiter: '\t',
         skipEmptyLines: true,
         chunk: (results: any, _: any) => {
-          console.log('CHUNKK', results.data)
           this.numInfections += results.data.length
           this.statusText = 'Reading infections: ' + this.numInfections
           for (const row of results.data) {
-            const { date, infected } = row
-            this.allInfections.push({ date, infected } as any)
+            this.allInfections.push({
+              date: row[`"date`],
+              home_lon: row[`"home_lon`],
+              home_lat: row[`"home_lat`],
+            } as any)
           }
         },
-        complete: (results: any, file: any) => {
-          console.log('COMPLETE', { results, file })
-          this.finishedLoadingInfections()
+        complete: () => {
+          this.buildDeckLayer()
         },
       })
-    },
-
-    async finishedLoadingInfections() {
-      console.log('DONE', { population: this.population, infections: this.allInfections })
-
-      // prepare population data
-      this.statusText = 'Building population lookup'
-      await this.$nextTick()
-
-      const personLookup = {} as any
-
-      for (const person of this.population) {
-        const xy = [parseFloat(person.homeX), parseFloat(person.homeY)]
-        const [lon, lat] = Coords.toLngLat(PROJECTION, xy)
-        personLookup[`${person.id}`] = Object.assign({ lat, lon }, person)
-      }
-
-      // prepare infection data
-      this.statusText = 'Joining infection data'
-      await this.$nextTick()
-
-      for (const infection of this.allInfections) {
-        const person = personLookup[infection.infected]
-        infection.coords = [person.lon, person.lat]
-      }
-
-      console.log('REALLY DONE', this.allInfections)
-      this.statusText = ''
-      this.buildDeckLayer()
     },
 
     buildDeckLayer() {
-      const deckOverlay = new DeckOverlay({
-        layers: [
-          new ScatterplotLayer({
-            id: 'pointlayer-1',
-            data: this.allInfections,
-            getFillColor: [64, 0, 200],
-            getRadius: 25,
-            getPosition: (d: InfectionRecord) => d.coords as any,
-            radiusScale: 1,
-            stroked: false,
-            filled: true,
-            autoHighlight: true,
-            highlightColor: [255, 255, 255],
-            opacity: 1.0,
-            pickable: true,
-            useDevicePixels: true,
-            transitions: {},
-            parameters: { depthTest: false },
-            glOptions: { fp64: false },
-            // updateTriggers: {}
-            // },
-            // filter shapes
-            // extensions: [new DataFilterExtension({ filterSize: 1 })],
-            // filterRange: [0, 1], // set filter to -1 to filter element out
-            // getFilterValue: (_: any, o: DeckObject) => {
-            //   return featureFilter[o.index]
-            // },
-          }) as any,
-        ],
-      })
+      this.statusText = 'Generating map...'
 
-      this.map.addControl(deckOverlay)
+      this.deckOverlay = new DeckOverlay({ layers: [] })
+
+      this.map.addControl(this.deckOverlay)
       this.map.addControl(new maplibregl.NavigationControl())
+      console.log('done done')
+      this.statusText = 'Infections Map'
+
+      this.updateLayers() // setTimeout(this.updateLayers, 1000)
     },
+
+    updateLayers() {
+      const layers = []
+
+      layers.push(
+        new ScatterplotLayer({
+          visible: this.view == MapView.points,
+          id: 'pointlayer-1',
+          data: this.allInfections,
+          getFillColor: [200, 0, 150],
+          getRadius: 10,
+          getPosition: (d: InfectionRecord) => [d.home_lon, d.home_lat],
+          radiusScale: 1,
+          stroked: false,
+          filled: true,
+          autoHighlight: true,
+          highlightColor: [255, 255, 255],
+          opacity: 0.1,
+          pickable: false,
+          useDevicePixels: true,
+          transitions: {},
+          parameters: { depthTest: false },
+          glOptions: { fp64: false },
+          // updateTriggers: {}
+          // },
+          // filter shapes
+          // extensions: [new DataFilterExtension({ filterSize: 1 })],
+          // filterRange: [0, 1], // set filter to -1 to filter element out
+          // getFilterValue: (_: any, o: DeckObject) => {
+          //   return featureFilter[o.index]
+          // },
+        }) as any
+      )
+
+      layers.push(
+        new HexagonLayer({
+          visible: this.view == MapView.hexagons,
+          id: 'HexagonLayer',
+          data: this.allInfections,
+          extruded: true,
+          getPosition: (d: any) => [d.home_lon, d.home_lat],
+          getColorWeight: 1,
+          getElevationWeight: 1,
+          elevationScale: 2,
+          radius: 200,
+          pickable: true,
+          opacity: 0.8,
+          coverage: 0.85,
+        })
+      )
+
+      this.deckOverlay._deck.setProps({ layers })
+    },
+
+    // new HeatmapLayer({
+    //   id: 'heatmaplayer-1',
+    //   // data: {
+    //   //   length: this.coordinates.length / 2,
+    //   //   attributes: {
+    //   //     getPosition: { size: 2, value: this.coordinates },
+    //   //   },
+    //   // },
+    //   data: this.allInfections,
+    //   getPosition: (d: any) => [d.home_lon, d.home_lat],
+    //   radiusPixels: 50,
+    //   weightsTextureSize: 512,
+    // }),
   },
 })
 </script>
@@ -214,8 +234,24 @@ b {
   flex-direction: column;
 }
 
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
+  gap: 0.5rem;
+}
+
+.flex1 {
+  flex: 1;
+}
+
 #mymap {
   flex: 1;
   width: 100%;
+}
+
+button.isActive {
+  background-color: #47f;
+  color: white;
 }
 </style>
